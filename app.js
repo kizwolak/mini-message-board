8d313eeaa5135e75b4905c3bcdd1bc1e1fccc232
kizwolak <ki.zwolak@gmail.com>
2023-04-21 00:56:38 -0300

    Finish + no secret

--- app.js
+++ app.js
@@ -3,11 +3,33 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const uri = process.env.SECRET_MONGODB_LINK;
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/new");
+const client = new MongoClient(uri);
+const PORT = process.env.PORT || 8080;
 
 var app = express();
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("messages");
+    const messages = database.collection("messages");
+    // Query for a movie that has the title 'Back to the Future'
+    const movie = await messages.find().toArray();
+    console.log(movie);
+    console.log("test");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+// run().catch(console.dir);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
